7256dfa5676a4d29fe471a9bc0c4934cd9cee9be
= <ladim1703@gmail.com>
2026-02-01 15:42:07 +0100

    migration(toys): add table toys

--- database/migrations/2026_02_01_143202_create_toys_table.php
+++ database/migrations/2026_02_01_143202_create_toys_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('toys', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('image_path')->nullable();
             $table->timestamps();
         });
     }
